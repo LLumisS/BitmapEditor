8eb7ed8f20b4c400b3e0abfaf8a573e3e63e4aa9
LLumisS <vasa16671@gmail.com>
2023-01-03 00:39:15 +0200

    Save Error Messages

---
+++ PictureEditor/ToolBar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureEditor
+{
+    internal class ToolBar
+    {
+        private int lightningData;
+        private int contrastData;
+        private int redData;
+        private int greenData;
+        private int blueData;
+
+        private ToolStripButton previous;
+
+        public void OnLightning(ToolStripButton tool)
+        {
+            if (previous != null) previous.Checked = false;
+            previous = tool;
+        }
+
+        public void OnContrast(ToolStripButton tool)
+        {
+            if (previous != null) previous.Checked = false;
+            previous = tool;
+        }
+
+        public void OnRGB(ToolStripButton tool)
+        {
+            if (previous != null) previous.Checked = false;
+            previous = tool;
+        }
+    }
+}
