af6266a6237f383793e3e8fa8b1b1eff8c84f7fd
LLumisS <vasa16671@gmail.com>
2023-01-05 03:34:43 +0200

    RGB dialog scroll + textbox

--- PictureEditor/Forms/RGB.Designer.cs
+++ PictureEditor/Forms/RGB.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PictureEditor.Forms
+﻿using System;
+
+namespace PictureEditor.Forms
 {
     partial class RGB
     {
@@ -36,6 +38,9 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
@@ -43,35 +48,38 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(56, 12);
+            this.trackBar1.Location = new System.Drawing.Point(68, 12);
             this.trackBar1.Maximum = 255;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(154, 45);
             this.trackBar1.TabIndex = 0;
             this.trackBar1.Value = 128;
+            this.trackBar1.Scroll += new System.EventHandler(this.ScrollChagedButton);
             // 
             // trackBar2
             // 
-            this.trackBar2.Location = new System.Drawing.Point(56, 63);
+            this.trackBar2.Location = new System.Drawing.Point(68, 63);
             this.trackBar2.Maximum = 255;
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Size = new System.Drawing.Size(154, 45);
             this.trackBar2.TabIndex = 1;
             this.trackBar2.Value = 128;
+            this.trackBar2.Scroll += new System.EventHandler(this.ScrollChagedButton);
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(56, 114);
+            this.trackBar3.Location = new System.Drawing.Point(68, 114);
             this.trackBar3.Maximum = 255;
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Size = new System.Drawing.Size(154, 45);
             this.trackBar3.TabIndex = 2;
             this.trackBar3.Value = 128;
+            this.trackBar3.Scroll += new System.EventHandler(this.ScrollChagedButton);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Location = new System.Drawing.Point(32, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 3;
@@ -80,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 75);
+            this.label2.Location = new System.Drawing.Point(23, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 4;
@@ -89,7 +97,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 124);
+            this.label3.Location = new System.Drawing.Point(31, 124);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 5;
@@ -97,7 +105,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 177);
+            this.button1.Location = new System.Drawing.Point(61, 176);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -107,7 +115,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 177);
+            this.button2.Location = new System.Drawing.Point(188, 176);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
@@ -115,11 +123,44 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.ExitButton);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "128";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextInputButton);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(64, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "128";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextInputButton);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(64, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "128";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextInputButton);
+            // 
             // RGB
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(233, 216);
+            this.ClientSize = new System.Drawing.Size(314, 226);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -148,5 +189,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- PictureEditor/Forms/RGB.cs
+++ PictureEditor/Forms/RGB.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,9 +25,40 @@ namespace PictureEditor.Forms
             image = _image;
         }
 
-        private void TextInput(object sender, EventArgs e)
+        private void TextInputButton(object sender, EventArgs e)
         {
+            TextBox textBox = sender as TextBox;
+            TrackBar trackBar = null;
 
+            if (Equals(textBox, textBox1)) trackBar = trackBar1;
+            else if (Equals(textBox, textBox2)) trackBar = trackBar2;
+            else trackBar = trackBar3;
+
+            try
+            {
+                trackBar.Value = Int32.Parse(textBox.Text);
+            }
+            catch
+            {
+                ScrollChaged(trackBar, textBox);
+            }
+        }
+
+        private void ScrollChagedButton(object sender, EventArgs e)
+        {
+            TrackBar trackBar = sender as TrackBar;
+            TextBox textBox = null;
+
+            if (Equals(trackBar, trackBar1)) textBox = textBox1;
+            else if (Equals(trackBar, trackBar2)) textBox = textBox2;
+            else textBox = textBox3;
+
+            textBox.Text = trackBar.Value.ToString();
+        }
+
+        private void ScrollChaged(TrackBar trackBar, TextBox textBox)
+        {
+            textBox.Text = trackBar.Value.ToString();
         }
 
         private void OKButton(object sender, EventArgs e)
